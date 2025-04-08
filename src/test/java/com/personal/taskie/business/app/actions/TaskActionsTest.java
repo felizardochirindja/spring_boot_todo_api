320b2734823ae9ecb9d65ab7ea9566d8e4fc9e97
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2025-04-08 19:47:05 +0200

    Add unit tests for TaskActions: implement tests for create, read by ID, read all, read all by user ID, and update methods.

---
+++ src/test/java/com/personal/taskie/business/app/actions/TaskActionsTest.java
@@ -0,0 +1,28 @@
+package com.personal.taskie.business.app.actions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskActionsTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void readById() {
+    }
+
+    @Test
+    void readAll() {
+    }
+
+    @Test
+    void readAllByUserId() {
+    }
+
+    @Test
+    void update() {
+    }
+}
