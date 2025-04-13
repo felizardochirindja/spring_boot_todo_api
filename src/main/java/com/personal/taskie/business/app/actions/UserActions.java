02fd7e4161fc83ea792bbfe1d10d7bba5270c051
felizardochirindja <felizardo.chirindja@gmail.com>
2025-04-13 10:20:41 +0200

    fix: remove unused createUserAction and related method from UserActions

--- src/main/java/com/personal/taskie/business/app/actions/UserActions.java
+++ src/main/java/com/personal/taskie/business/app/actions/UserActions.java
@@ -1,9 +1,7 @@
 package com.personal.taskie.business.app.actions;
 
 import com.personal.taskie.adapters.repos.UserRepository;
-import com.personal.taskie.business.app.ports.input.UserCreator;
 import com.personal.taskie.business.entities.exceptions.EntityNotFoundException;
-import com.personal.taskie.business.app.params.input.CreateUserInput;
 import com.personal.taskie.business.entities.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +11,6 @@ import org.springframework.stereotype.Service;
 public final class UserActions {
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private UserCreator createUserAction;
-
-    public User create(CreateUserInput params) {
-        return createUserAction.execute(params);
-    }
 
     public User readById(int id) {
         return userRepository.findById(id)
