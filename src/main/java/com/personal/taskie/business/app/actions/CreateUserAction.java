51618b11914c132c04017dad10b05760f18b945f
felizardochirindja <felizardo.chirindja@gmail.com>
2025-04-15 15:12:32 +0200

    feat: enhance user creation logging in CreateUserAction

--- src/main/java/com/personal/taskie/business/app/actions/CreateUserAction.java
+++ src/main/java/com/personal/taskie/business/app/actions/CreateUserAction.java
@@ -4,6 +4,8 @@ import com.personal.taskie.adapters.repos.UserRepository;
 import com.personal.taskie.business.app.params.input.CreateUserInput;
 import com.personal.taskie.business.app.ports.input.UserCreator;
 import com.personal.taskie.business.entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +13,31 @@ import org.springframework.stereotype.Service;
 public final class CreateUserAction implements UserCreator {
     @Autowired
     private UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CreateUserAction.class);
 
     public User execute(CreateUserInput params) {
-        userRepository.findByEmail(params.email())
-                .ifPresent(user -> {
-                    throw new RuntimeException("user already exists!");
-                });
+        logger.atInfo()
+                .setMessage("Trying to create user")
+                .addKeyValue("email", params.email())
+                .log();
 
-        return userRepository.save(params.createUser());
+        userRepository.findByEmail(params.email()).ifPresent(user -> {
+            logger.atWarn()
+                    .setMessage("User already exists")
+                    .addKeyValue("email", params.email())
+                    .log();
+
+            throw new RuntimeException("User already exists!");
+        });
+
+        User createdUser = userRepository.save(params.createUser());
+
+        logger.atInfo()
+                .setMessage("User created successfully")
+                .addKeyValue("userId", createdUser.getId())
+                .addKeyValue("email", createdUser.getEmail())
+                .log();
+
+        return createdUser;
     }
 }
