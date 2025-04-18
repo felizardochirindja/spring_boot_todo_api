116b2338739ef52d60ef069f7c196ca6d34dbcd1
felizardochirindja <felizardo.chirindja@gmail.com>
2025-04-18 14:12:51 +0200

    ref: reorganize migration execution order

--- src/main/resources/db/migration/V2__create_users_table.sql
+++ src/main/resources/db/migration/V2__create_users_table.sql
@@ -3,6 +3,8 @@ create table if not exists users(
     name varchar(255) not null,
     email varchar(255) not null,
     password varchar(255) not null,
+    role_id int not null,
     constraint pk_users primary key (id),
+    constraint fk_users_roles foreign key (role_id) references roles(id),
     unique index unq_email (email asc) visible
 ) engine=innodb default character set = utf8mb4 collate  utf8mb4_0900_ai_ci;
