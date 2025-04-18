f8583378cb391784a6e573a49ec6d5a7b3d0b1ad
felizardochirindja <felizardo.chirindja@gmail.com>
2025-04-18 14:18:55 +0200

    feat: add role field to SignupPayload and update createActionParams method

--- src/main/java/com/personal/task/infra/platforms/api/payloads/SignupPayload.java
+++ src/main/java/com/personal/task/infra/platforms/api/payloads/SignupPayload.java
@@ -1,6 +1,7 @@
 package com.personal.task.infra.platforms.api.payloads;
 
 import com.personal.task.business.app.params.input.SignupInput;
+import com.personal.task.business.entities.Role;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -13,9 +14,11 @@ public record SignupPayload(
         String email,
         @NotNull
         @NotBlank
-        String password
+        String password,
+        @NotNull
+        Role.Values role
 ) {
         public SignupInput createActionParams() {
-                return new SignupInput(name, email, password);
+                return new SignupInput(name, email, password, role);
         }
 }
