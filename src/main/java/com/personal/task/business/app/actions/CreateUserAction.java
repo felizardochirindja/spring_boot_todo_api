d9eb0531fc39846edc5afbb76c70926897f8a6a1
felizardochirindja <felizardo.chirindja@gmail.com>
2025-04-21 09:15:41 +0200

    feat: add transaction to execute method declaratively

--- src/main/java/com/personal/task/business/app/actions/CreateUserAction.java
+++ src/main/java/com/personal/task/business/app/actions/CreateUserAction.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public final class CreateUserAction implements UserCreator {
@@ -20,6 +21,7 @@ public final class CreateUserAction implements UserCreator {
     private RoleRepository roleRepository;
     private static final Logger logger = LoggerFactory.getLogger(CreateUserAction.class);
 
+    @Transactional
     public User execute(CreateUserInput params) {
         logger.atInfo()
                 .setMessage("Trying to create user!")
