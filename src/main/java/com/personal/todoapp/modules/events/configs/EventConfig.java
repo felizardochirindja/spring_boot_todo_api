f96a231986d0600e35fb11c7c6042cb37dddf41f
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-07 16:31:55 +0200

    ref: move event configs from AppConfig to EventConfig

---
+++ src/main/java/com/personal/todoapp/modules/events/configs/EventConfig.java
@@ -0,0 +1,29 @@
+package com.personal.todoapp.modules.events.configs;
+
+import com.personal.todoapp.modules.events.handlers.EventPublisher;
+import com.personal.todoapp.modules.events.handlers.adapters.KafkaEventPublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!test")
+public class EventConfig {
+    @Value("${app.message_broker.name:kafka}")
+    private String brokerName;
+
+    @Autowired(required = false)
+    private KafkaEventPublisher kafkaPublisher;
+
+    @Bean
+    @Primary
+    public EventPublisher eventPublisher() {
+        return switch (brokerName.toLowerCase()) {
+            case "kafka" -> kafkaPublisher;
+            default -> throw new IllegalArgumentException("broker name not supported: " + brokerName);
+        };
+    }
+}
--- src/main/java/com/personal/todoapp/modules/shared/configs/AppConfig.java
+++ src/main/java/com/personal/todoapp/modules/shared/configs/AppConfig.java
@@ -4,14 +4,9 @@ import com.personal.todoapp.modules.user.business.app.CreateUserAction;
 import com.personal.todoapp.modules.user.business.app.ports.input.UserCreator;
 import com.personal.todoapp.modules.task.business.app.ports.output.remotetask.RemoteTaskSyncFetcher;
 import com.personal.todoapp.modules.task.infra.adapters.libs.DummyJsonTaskSyncFetcherByWebClient;
-import com.personal.todoapp.modules.events.handlers.EventPublisher;
-import com.personal.todoapp.modules.events.handlers.adapters.KafkaEventPublisher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableCaching
@@ -24,20 +19,5 @@ public class AppConfig {
     @Bean
     public UserCreator userCreator() {
         return new CreateUserAction();
-    }
-
-    @Value("${app.message_broker.name:kafka}")
-    private String brokerName;
-
-    @Autowired(required = false)
-    private KafkaEventPublisher kafkaPublisher;
-
-    @Bean
-    @Primary
-    public EventPublisher eventPublisher() {
-        return switch (brokerName.toLowerCase()) {
-            case "kafka" -> kafkaPublisher;
-            default -> throw new IllegalArgumentException("broker name not supported: " + brokerName);
-        };
     }
 }
