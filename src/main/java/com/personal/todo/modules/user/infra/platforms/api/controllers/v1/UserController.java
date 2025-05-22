24c80dc31471bb00dcace90335fcb74f1bac3abc
felizardochirindja <felizardo.chirindja@gmail.com>
2025-05-22 16:06:06 +0200

    feat: enhance readAllRemoteTasksByUserId to validate user ID against authenticated user

--- src/main/java/com/personal/todo/modules/user/infra/platforms/api/controllers/v1/UserController.java
+++ src/main/java/com/personal/todo/modules/user/infra/platforms/api/controllers/v1/UserController.java
@@ -1,5 +1,6 @@
 package com.personal.todo.modules.user.infra.platforms.api.controllers.v1;
 
+import com.personal.todo.modules.auth.business.entities.AuthUser;
 import com.personal.todo.modules.task.business.app.params.output.ReadRemoteTasksOutput;
 import com.personal.todo.modules.task.business.app.actions.TaskActions;
 import com.personal.todo.modules.user.business.app.UserActions;
@@ -10,20 +11,26 @@ import com.personal.todo.modules.task.infra.platforms.api.controllers.v1.respons
 import com.personal.todo.modules.user.infra.platforms.api.controllers.v1.responses.UserApi;
 import com.personal.todo.modules.user.infra.platforms.api.controllers.v1.responses.UserApiResponse;
 import jakarta.validation.constraints.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-public final class UserController {
+public class UserController {
     @Autowired
     private UserActions userActions;
     @Autowired
     private TaskActions taskActions;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
 
     @GetMapping("/{id}")
     public ResponseEntity<UserApiResponse> readById(@PathVariable Integer id) {
@@ -43,11 +50,27 @@ public final class UserController {
         return ResponseEntity.ok(taskActions.readAllByUserId(id));
     }
 
-    @GetMapping("/tasks/remote")
-    public ResponseEntity<RemoteTasksApiResponse> readAllRemoteTasksByUserId() {
-        int userId = 1;
+    @GetMapping("{id}/tasks/remote")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<RemoteTasksApiResponse> readAllRemoteTasksByUserId(
+            @PathVariable @NotNull Integer id,
+            Authentication auth
+    ) {
+        String authEmail = ((AuthUser) auth.getPrincipal()).getUsername();
+        User user = userActions.readByEmail(authEmail);
 
-        ReadRemoteTasksOutput output = taskActions.readRemoteTasksByUserId(userId);
+        if (!user.getId().equals(id)) {
+            logger.atWarn()
+                    .setMessage("auth user id does not match the path user id")
+                    .addKeyValue("authUserId", user.getId())
+                    .addKeyValue("pathUserId", id)
+                    .log();
+
+
+            throw new IllegalArgumentException("auth user id does not match the path user id");
+        }
+
+        ReadRemoteTasksOutput output = taskActions.readRemoteTasksByUserId(id);
 
         List<RemoteTaskApi> taskList = output.tasks()
                 .stream()
