c89e7bc5a0136afecad3c4e58b24d3e7872a9c83
felizardochirindja <felizardo.chirindja@gmail.com>
2025-06-14 12:03:03 +0200

    refactor: remove unused EventMessaging interface

--- src/main/java/com/personal/todo/modules/events/entities/EventMessaging.java
+++
@@ -1,8 +0,0 @@
-package com.personal.todo.modules.events.entities;
-
-import java.time.LocalDateTime;
-
-public interface EventMessaging {
-    String getName();
-    LocalDateTime getTimestamp();
-}
