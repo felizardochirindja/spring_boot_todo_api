1e4e77bfa2c8d9cea0446c430b9d7c3eff4f3c73
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-05-16 14:01:15 +0200

    feat: add security scheme configuration for Swagger API documentation

--- src/main/java/com/personal/todo/modules/shared/configs/SwaggerConfig.java
+++ src/main/java/com/personal/todo/modules/shared/configs/SwaggerConfig.java
@@ -1,11 +1,19 @@
 package com.personal.todo.modules.shared.configs;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
 public class SwaggerConfig {
     @Bean
     public GroupedOpenApi v1() {
