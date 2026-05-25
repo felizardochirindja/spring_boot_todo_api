70fd4383adb39bdf2e1bf104751fa74c106349f2
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-05-25 14:00:32 +0200

    feat: reorganize AppConfig to enable caching and add Hazelcast cache manager

--- src/main/java/com/personal/todo/modules/shared/configs/AppConfig.java
+++ src/main/java/com/personal/todo/modules/shared/configs/AppConfig.java
@@ -2,16 +2,20 @@ package com.personal.todo.modules.shared.configs;
 
 import com.personal.todo.modules.user.business.app.CreateUserAction;
 import com.personal.todo.modules.user.business.app.ports.input.UserCreator;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
 import com.personal.todo.modules.task.business.app.ports.output.remotetask.RemoteTaskSyncFetcher;
 import com.personal.todo.modules.task.infra.adapters.libs.DummyJsonTaskSyncFetcherByWebClient;
+
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableCaching
 @EnableScheduling
+@EnableCaching
 public class AppConfig {
     @Bean
     public RemoteTaskSyncFetcher remoteTaskSyncFetcher() {
@@ -21,5 +25,10 @@ public class AppConfig {
     @Bean
     public UserCreator userCreator() {
         return new CreateUserAction();
+    }
+
+    @Bean
+    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
+        return new HazelcastCacheManager(hazelcastInstance);
     }
 }
