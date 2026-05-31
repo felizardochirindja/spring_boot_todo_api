ceeaf9fab3779068730b188a15190fb5e1cee935
Felizardo Chirindja <felizardo.chirindja@gmail.com>
2026-05-31 12:32:56 +0200

    refactor: rearrange scheduling and caching annotations in AppConfig

--- src/main/java/com/personal/todo/modules/shared/configs/AppConfig.java
+++ src/main/java/com/personal/todo/modules/shared/configs/AppConfig.java
@@ -2,20 +2,17 @@ package com.personal.todo.modules.shared.configs;
 
 import com.personal.todo.modules.user.business.app.CreateUserAction;
 import com.personal.todo.modules.user.business.app.ports.input.UserCreator;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.spring.cache.HazelcastCacheManager;
 import com.personal.todo.modules.task.business.app.ports.output.remotetask.RemoteTaskSyncFetcher;
 import com.personal.todo.modules.task.infra.adapters.libs.DummyJsonTaskSyncFetcherByWebClient;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
-@EnableScheduling
 @EnableCaching
+@EnableScheduling
 public class AppConfig {
     @Bean
     public RemoteTaskSyncFetcher remoteTaskSyncFetcher() {
@@ -25,10 +22,5 @@ public class AppConfig {
     @Bean
     public UserCreator userCreator() {
         return new CreateUserAction();
-    }
-
-    @Bean
-    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
-        return new HazelcastCacheManager(hazelcastInstance);
     }
 }
